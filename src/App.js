5e43f3f1524e75994f3dd1880c2d55312afe0623
Nico Mordvini <nico.mordvini@yandex.ru>
2019-02-17 23:49:32 +0400

    Comment before calling configurations

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Section from './components/Section'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => {
+  return (
+      <div className="all_window">
+        <Header></Header>
+        <Section></Section>
+        <Section></Section>
+        <Section></Section>
+        <Section></Section>
+        <Section></Section>
+        <Footer></Footer>
       </div>
-    );
-  }
+  );
 }
 
 export default App;
